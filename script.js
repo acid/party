a6c8923675a94c995f835f9442e696e5181081ce
Daniel Schweighoefer <daniel@netsteward.net>
2021-05-28 17:24:15 +0200

    update script

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
-//# sourceMappingURL=script.js.map
+let introPopup;
+
+// Open the popup when we enter a given zone
+introPopup = WA.onEnterZone('myZone', () => {
+    WA.openPopup("intro", "Welcome to my partyspace! There's a jitsi area in the library and the kitchen. Can you find the treasure chest on every map? :) You can zoom out to see more of the map.", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('myZone', () => {
+    introPopup.close();
+});
