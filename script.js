d0649f8b52de6d107c083e47d8657f4bbed81c8b
Daniel Schweighoefer <daniel@netsteward.net>
2021-05-28 16:59:25 +0200

    fix script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let introPopup;
 
 // Open the popup when we enter a given zone
 introPopup = WA.onEnterZone('myZone', () => {
-    WA.openPopup("intro", 'Welcome to my partyspace! There's a jitsi area in the library and the kitchen. Can you find the treasure chest on every map? :) You can zoom out to see more of the map.', [{
+    WA.openPopup("intro", "Welcome to my partyspace! There's a jitsi area in the library and the kitchen. Can you find the treasure chest on every map? :) You can zoom out to see more of the map.", [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
